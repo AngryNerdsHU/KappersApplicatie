1c5c5a9bbd9c08cc9982f0220138eca3518f6822
PuckBakker <70576609+PuckBakker@users.noreply.github.com>
2020-12-09 09:44:48 +0100

    Ombouwen Djangoproject Jan

--- Backend/Kappersapplicatie/hairdresser/serializers.py
+++ Backend/Kappersapplicatie/hairdresser/serializers.py
@@ -2,39 +2,50 @@ from rest_framework import serializers
 from .models import Article, Author, Appointment, Customer
 
 
-class AuthorSerializer(serializers.ModelSerializer):
+class GeslachtSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Author
+        model = Geslacht
         fields = ('id',
-                  'name',
-                  'email',)
+                  'man',
+                  'vrouw',)
 
 
-class ArticleSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Article
-        fields = ('id',
-                  'title',
-                  'description',
-                  'author_id')
 
 
-class AppointmentSerializer(serializers.ModelSerializer):
+# class AuthorSerializer(serializers.ModelSerializer):
 
-    class Meta:
-        model = Appointment
-        fields = ('id',
-                  'date',
-                  'description',
-                  'customer_id')
+#     class Meta:
+#         model = Author
+#         fields = ('id',
+#                   'name',
+#                   'email',)
 
 
-class CustomerSerializer(serializers.ModelSerializer):
+# class ArticleSerializer(serializers.ModelSerializer):
 
-    class Meta:
-        model = Customer
-        fields = ('id',
-                  'name',
-                  'email')
+#     class Meta:
+#         model = Article
+#         fields = ('id',
+#                   'title',
+#                   'description',
+#                   'author_id')
+
+
+# class AppointmentSerializer(serializers.ModelSerializer):
+
+#     class Meta:
+#         model = Appointment
+#         fields = ('id',
+#                   'date',
+#                   'description',
+#                   'customer_id')
+
+
+# class CustomerSerializer(serializers.ModelSerializer):
+
+#     class Meta:
+#         model = Customer
+#         fields = ('id',
+#                   'name',
+#                   'email')
--- Backend/Kappersapplicatie/hairdresser/views.py
+++ Backend/Kappersapplicatie/hairdresser/views.py
@@ -5,145 +5,175 @@ from rest_framework import status
 from django.shortcuts import get_object_or_404
 from django.http.response import JsonResponse
 
-from .models import Article, Author, Appointment, Customer
+from .models import Article, Author, Appointment, Customer, Geslacht
 from .serializers import ArticleSerializer, AuthorSerializer, ArticleSerializer, AppointmentSerializer, CustomerSerializer
 
 
-class AuthorView(APIView):
-    def get(self, request):
-        data = Author.objects.all()
-        serializer = AuthorSerializer(data, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    def post(self, request):
-        json = JSONParser().parse(request)
-        serializer = AuthorSerializer(data=json)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def put(self, request, pk):
-        try:
-            data = Author.objects.get(pk=pk)
-        except Author.DoesNotExist:
-            return JsonResponse({'message': 'Item does not exist'}, status=status.HTTP_404_NOT_FOUND)
-        json = JSONParser().parse(request)
-        serializer = AuthorSerializer(data, data=json)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        try:
-            data = Author.objects.get(pk=pk)
-        except Author.DoesNotExist:
-            return JsonResponse({'message': 'Item does not exist'}, status=status.HTTP_404_NOT_FOUND)
-        data.delete()
-        return JsonResponse({'message': 'Item deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
+class GeslachtView(APIView):
+    def request_page(request):
+    if(request.GET.get('mybtn')):
+        mypythoncode.mypythonfunction( int(request.GET.get('mytextbox')) )
+    return render(request,'myApp/templateHTML.html')
 
 
-class ArticleView(APIView):
-    def get(self, request):
-        data = Article.objects.all()
-        serializer = ArticleSerializer(data, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    def post(self, request):
-        json = JSONParser().parse(request)
-        serializer = ArticleSerializer(data=json)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def put(self, request, pk):
-        try:
-            data = Article.objects.get(pk=pk)
-        except Author.DoesNotExist:
-            return JsonResponse({'message': 'Item does not exist'}, status=status.HTTP_404_NOT_FOUND)
-        json = JSONParser().parse(request)
-        serializer = ArticleSerializer(data, data=json)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        try:
-            data = Article.objects.get(pk=pk)
-        except Article.DoesNotExist:
-            return JsonResponse({'message': 'Item does not exist'}, status=status.HTTP_404_NOT_FOUND)
-        data.delete()
-        return JsonResponse({'message': 'Item deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
 
 
-class AppointmentView(APIView):
-    def get(self, request):
-        data = Appointment.objects.all()
-        serializer = AppointmentSerializer(data, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    def post(self, request):
-        json = JSONParser().parse(request)
-        serializer = AppointmentSerializer(data=json)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def put(self, request, pk):
-        try:
-            data = Appointment.objects.get(pk=pk)
-        except Appointment.DoesNotExist:
-            return JsonResponse({'message': 'Item does not exist'}, status=status.HTTP_404_NOT_FOUND)
-        json = JSONParser().parse(request)
-        serializer = AppointmentSerializer(data, data=json)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        try:
-            data = Appointment.objects.get(pk=pk)
-        except Appointment.DoesNotExist:
-            return JsonResponse({'message': 'Item does not exist'}, status=status.HTTP_404_NOT_FOUND)
-        data.delete()
-        return JsonResponse({'message': 'Item deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
 
 
-class CustomerView(APIView):
-    def get(self, request):
-        data = Customer.objects.all()
-        serializer = CustomerSerializer(data, many=True)
-        return JsonResponse(serializer.data, safe=False)
 
-    def post(self, request):
-        json = JSONParser().parse(request)
-        serializer = CustomerSerializer(data=json)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def put(self, request, pk):
-        try:
-            data = Customer.objects.get(pk=pk)
-        except Customer.DoesNotExist:
-            return JsonResponse({'message': 'Item does not exist'}, status=status.HTTP_404_NOT_FOUND)
-        json = JSONParser().parse(request)
-        serializer = CustomerSerializer(data, data=json)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk):
-        try:
-            data = Customer.objects.get(pk=pk)
-        except Customer.DoesNotExist:
-            return JsonResponse({'message': 'Item does not exist'}, status=status.HTTP_404_NOT_FOUND)
-        data.delete()
-        return JsonResponse({'message': 'Item deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class AuthorView(APIView):
+#     def get(self, request):
+#         data = Author.objects.all()
+#         serializer = AuthorSerializer(data, many=True)
+#         return JsonResponse(serializer.data, safe=False)
+
+#     def post(self, request):
+#         json = JSONParser().parse(request)
+#         serializer = AuthorSerializer(data=json)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+#         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def put(self, request, pk):
+#         try:
+#             data = Author.objects.get(pk=pk)
+#         except Author.DoesNotExist:
+#             return JsonResponse({'message': 'Item does not exist'}, status=status.HTTP_404_NOT_FOUND)
+#         json = JSONParser().parse(request)
+#         serializer = AuthorSerializer(data, data=json)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data)
+#         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def delete(self, request, pk):
+#         try:
+#             data = Author.objects.get(pk=pk)
+#         except Author.DoesNotExist:
+#             return JsonResponse({'message': 'Item does not exist'}, status=status.HTTP_404_NOT_FOUND)
+#         data.delete()
+#         return JsonResponse({'message': 'Item deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
+
+
+# class ArticleView(APIView):
+#     def get(self, request):
+#         data = Article.objects.all()
+#         serializer = ArticleSerializer(data, many=True)
+#         return JsonResponse(serializer.data, safe=False)
+
+#     def post(self, request):
+#         json = JSONParser().parse(request)
+#         serializer = ArticleSerializer(data=json)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+#         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def put(self, request, pk):
+#         try:
+#             data = Article.objects.get(pk=pk)
+#         except Author.DoesNotExist:
+#             return JsonResponse({'message': 'Item does not exist'}, status=status.HTTP_404_NOT_FOUND)
+#         json = JSONParser().parse(request)
+#         serializer = ArticleSerializer(data, data=json)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data)
+#         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def delete(self, request, pk):
+#         try:
+#             data = Article.objects.get(pk=pk)
+#         except Article.DoesNotExist:
+#             return JsonResponse({'message': 'Item does not exist'}, status=status.HTTP_404_NOT_FOUND)
+#         data.delete()
+#         return JsonResponse({'message': 'Item deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
+
+
+# class AppointmentView(APIView):
+#     def get(self, request):
+#         data = Appointment.objects.all()
+#         serializer = AppointmentSerializer(data, many=True)
+#         return JsonResponse(serializer.data, safe=False)
+
+#     def post(self, request):
+#         json = JSONParser().parse(request)
+#         serializer = AppointmentSerializer(data=json)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+#         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def put(self, request, pk):
+#         try:
+#             data = Appointment.objects.get(pk=pk)
+#         except Appointment.DoesNotExist:
+#             return JsonResponse({'message': 'Item does not exist'}, status=status.HTTP_404_NOT_FOUND)
+#         json = JSONParser().parse(request)
+#         serializer = AppointmentSerializer(data, data=json)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data)
+#         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def delete(self, request, pk):
+#         try:
+#             data = Appointment.objects.get(pk=pk)
+#         except Appointment.DoesNotExist:
+#             return JsonResponse({'message': 'Item does not exist'}, status=status.HTTP_404_NOT_FOUND)
+#         data.delete()
+#         return JsonResponse({'message': 'Item deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
+
+
+# class CustomerView(APIView):
+#     def get(self, request):
+#         data = Customer.objects.all()
+#         serializer = CustomerSerializer(data, many=True)
+#         return JsonResponse(serializer.data, safe=False)
+
+#     def post(self, request):
+#         json = JSONParser().parse(request)
+#         serializer = CustomerSerializer(data=json)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+#         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def put(self, request, pk):
+#         try:
+#             data = Customer.objects.get(pk=pk)
+#         except Customer.DoesNotExist:
+#             return JsonResponse({'message': 'Item does not exist'}, status=status.HTTP_404_NOT_FOUND)
+#         json = JSONParser().parse(request)
+#         serializer = CustomerSerializer(data, data=json)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data)
+#         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def delete(self, request, pk):
+#         try:
+#             data = Customer.objects.get(pk=pk)
+#         except Customer.DoesNotExist:
+#             return JsonResponse({'message': 'Item does not exist'}, status=status.HTTP_404_NOT_FOUND)
+#         data.delete()
+#         return JsonResponse({'message': 'Item deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
