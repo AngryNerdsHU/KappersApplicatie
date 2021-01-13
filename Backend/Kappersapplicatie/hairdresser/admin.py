676009c6d8483ca3ce45c3c824bd21fec5af5e2c
pjotrkuiper <pjotr.kuiper@student.hu.nl>
2021-01-13 14:15:12 +0100

    aanpassingen

--- Backend/Kappersapplicatie/hairdresser/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Article, Author, Customer, Appointment
-
-admin.site.register(Article)
-admin.site.register(Author)
-admin.site.register(Customer)
-admin.site.register(Appointment)
--- Backend/Kappersapplicatie/hairdresser/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HairdresserConfig(AppConfig):
-    name = 'hairdresser'
