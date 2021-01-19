267188de6a14fdd416cb0dd5e3267306c204c3ba
PuckBakker <70576609+PuckBakker@users.noreply.github.com>
2021-01-19 16:15:30 +0100

    Logout button

--- Front-end/Login Kapper/login.js
+++ Front-end/Login Kapper/login.js
@@ -1,17 +1,26 @@
+// listen for auth status changes
+auth.onAuthStateChanged(user => {
+    if (user) {
+        console.log('user logged in: ', user);
+        window.location.assign("./test/test.html");
+    } else {
+        console.log('user logged out');
+    }
+});
+
 // login
 const loginForm = document.querySelector('#form');
 loginForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  
-  // get user info
-  const email = loginForm['login-email'].value;
-  const password = loginForm['login-password'].value;
-   
-  // log the user in
-  auth.signInWithEmailAndPassword(email, password).then((cred) => {
-    console.log(cred.user);
-    // close the signup modal & reset form
-    loginForm.reset();
-  });
+    e.preventDefault();
+
+    // get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    // log the user in
+    auth.signInWithEmailAndPassword(email, password).then((cred) => {
+        // close the signup modal & reset form
+        loginForm.reset();
+    });
 
 });
--- Front-end/Login Kapper/registreren/scripts/auth.js
+++ Front-end/Login Kapper/registreren/scripts/auth.js
@@ -10,7 +10,6 @@ signupForm.addEventListener('submit', (e) => {
 
     // sign up the user
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        console.log(cred.user);
         signupForm.reset()
     });
 });
